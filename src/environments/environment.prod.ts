9fa95f90daa1967d5623c19b56b6874b74e3758c
mvasylchikov <mailbox9112@gmail.com>
2018-02-04 13:51:09 +0200

    page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  quiz: 'quiz',
+  save_result_quiz: 'save_result_quiz',
+  delete_quiz: 'delete_quiz',
+  get_quiz: 'get_quiz',
+  log: 'log',
+  host: 'https://mask-sh-4.github.io/reloadly/'
 };
